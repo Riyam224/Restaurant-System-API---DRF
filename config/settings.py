862b844e67baa0855ce2c5ed2b2e8eaf879be715
Riyam <cs50riyam211@gmail.com>
2026-01-04 19:57:27 +0300

    fix bug in settings

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
-import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
@@ -127,18 +128,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": "/tmp/db.sqlite3",
-#     }
-# }
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get(
-            "postgresql://postgres:uoJyNrdkUZstDJfMJZfThtVghCSJVWNA@postgres.railway.internal:5432/railway"
-        )
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
