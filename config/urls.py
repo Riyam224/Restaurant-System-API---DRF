fcbb5b0154bb24948a46bc0c3cface4ea15a43a3
Riyam <cs50riyam211@gmail.com>
2026-01-04 21:53:07 +0300

    fix bugs and improve code

--- config/urls.py
+++ config/urls.py
@@ -1,30 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # 🔐 Accounts APIs
+    # APIs
     path("api/v1/", include("accounts.urls")),
-    # 🍽 Menu APIs
     path("api/v1/", include("menu.urls")),
-    # 🛒 Cart APIs
     path("api/v1/", include("cart.urls")),
-    # 📦 Orders APIs
     path("api/v1/", include("orders.urls")),
-]
-
-# 📚 Swagger / OpenAPI
-urlpatterns += [
+    # Docs
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
