8d5b1ac4bb6349ad36b90542f8be0748d7402b31
Riyam <cs50riyam211@gmail.com>
2026-01-15 20:03:49 +0300

    add management folder to create superuser

---
+++ accounts/management/commands/create_superuser_if_not_exists.py
@@ -0,0 +1,28 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create superuser if it doesn't exist"
+
+    def handle(self, *args, **kwargs):
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("Superuser env vars missing")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Superuser already exists")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+        )
+        self.stdout.write("Superuser created")
