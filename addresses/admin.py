e390a52f897c94fb91eb5ec1514db5bf0d0d1567
Riyam <cs50riyam211@gmail.com>
2026-01-15 19:17:04 +0300

    fix

--- addresses/admin.py
+++ addresses/admin.py
@@ -6,7 +6,7 @@ from .models import Address
 class AddressAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "label", "city", "street", "created_at")
     list_filter = ("city", "created_at")
-    search_fields = ("user__username", "user__email", "label", "city", "street")
+    search_fields = ("user__email", "label", "city", "street")
     readonly_fields = ("created_at",)
     ordering = ("-created_at",)
 
