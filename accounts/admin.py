32a83f28c4ee7a439b3a427f70aa1ce53507984c
Riyam <cs50riyam211@gmail.com>
2026-02-03 17:05:11 +0300

    fix admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import PasswordResetOTP
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @admin.register(PasswordResetOTP)
 class PasswordResetOTPAdmin(admin.ModelAdmin):
-    list_display = ['user', 'otp', 'created_at', 'expires_at', 'is_used']
-    list_filter = ['is_used', 'created_at']
-    search_fields = ['user__email', 'user__username', 'otp']
-    readonly_fields = ['created_at']
-    ordering = ['-created_at']
-
+    list_display = ["user", "otp", "created_at", "expires_at", "is_used"]
+    list_filter = ["is_used", "created_at"]
+    search_fields = ["user__email", "user__username", "otp"]
+    readonly_fields = ["created_at"]
+    ordering = ["-created_at"]
